cee9ddec096a429a4b57b93eeb24249081573b55
jeanc <jeanc@DESKTOP-G3VBIB9>
2024-04-24 00:11:42 -0300

    testando operadores matematicos e o scan

---
+++ src/main/java/OperacaoMat.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+//@author jeanc
+public class OperacaoMat {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        float num1, num2;
+        
+        System.out.print("Digite o primeiro numero: ");
+        num1 = scan.nextFloat();
+        System.out.print("Digite o segundo numero: ");
+        num2 = scan.nextFloat();
+        
+        float soma = (num1 + num2);
+        float subtracao = (num1 - num2);
+        float div = (num1/num2);
+        float mult = (num1*num2);
+        
+        System.out.printf("\n%.2f + %.2f = %.2f\n", num1, num2, soma);
+        System.out.printf("%.2f - %.2f = %.2f\n", num1, num2, subtracao);
+        System.out.printf("%.2f x %.2f = %.2f\n", num1, num2, mult);
+        System.out.printf("%.2f : %.2f = %.2f\n", num1, num2, div);
+    }
+}
