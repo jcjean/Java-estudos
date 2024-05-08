9df6312f01a9822e618069cb47ab812a9d1e31b6
jcjean <jcjean@DESKTOP-G3VBIB9>
2024-05-07 22:43:07 -0300

    tabuleiro do jogo da velha (será desenvolvido mais pra frente)

---
+++ src/main/java/JogoDaVelha.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author jeanc
+ */
+
+//tabuleiro do jogo
+public class JogoDaVelha {
+    public static void main(String[] args){
+        System.out.println("___|___|___\n"+
+                           "___|___|___\n"+
+                           "   |   |   \n");
+    }
+}
